319d4c7322f86fc570d2f7808320e93f6b4a4999
cristh34 <cristhiansaudin@gmail.com>
2018-12-06 03:02:36 -0400

    Creacion de lista de Post

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class PageController extends Controller
 {
    public function blog(){
 
-   	return 'blog';
+   	$posts = Post::orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+
+   	return view('web.posts', compact('posts'));
 
    }
 }
